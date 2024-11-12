169d8d05e29a21b53a7b2b28935230ba5d24923b
Artem Makovskyy <photomak79@gmail.com>
2024-11-12 11:32:51 +0200

    simple app init

---
+++ src/main/java/com/example/webapplication/Controller.java
@@ -0,0 +1,15 @@
+package com.example.webapplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class Controller {
+
+    @GetMapping
+    public String test(){
+        return "The api is working!";
+    }
+}
